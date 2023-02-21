6ece85e997ba69c9b4c6c44396731e1ff0c14538
Konrad44444 <konrad_warzecha@wp.pl>
2023-02-21 19:18:20 +0100

    border shadow, ending layout to test was started

--- app/src/main/java/com/example/learnapp/LearnActivity.java
+++ app/src/main/java/com/example/learnapp/LearnActivity.java
@@ -3,33 +3,40 @@ package com.example.learnapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LearnActivity extends AppCompatActivity {
     private final String MESSAGE = "tableName";
     private SetsDb db = new SetsDb(this);
-    private Button readyButton, hintButton;
+    private Button readyButton, hintButton, goToMenu;
     private EditText definitionTextEdit;
-    private TextView descriptionTextView, correctAfterCheck, insertedAfterCheck, remainText;
+    private TextView descriptionTextView, correctAfterCheck, insertedAfterCheck, remainText, wordsCnt, errCnt, mostErrDef, mostErrDesc;
     private HashMap<String, String> defDescList = new HashMap<>();
     private ArrayList<String> definitionsArray = new ArrayList<>();
+    private ArrayList<String> definitionsArrayCopy = new ArrayList<>();
     private String tableName;
-    private RelativeLayout check, mainLay;
+    private RelativeLayout check, mainLay, endLay;
     int currentWord = -1, notCorrectCnt = 0;
     boolean wasGuessed = false;
+    int[] errorsCount;
 
 
     @Override
@@ -49,19 +56,35 @@ public class LearnActivity extends AppCompatActivity {
         remainText = findViewById(R.id.remainText); //wyświetlanie liczby pozostałych słów
         hintButton = findViewById(R.id.hintButton); //podpowiedź - pierwsza litera
 
+        //ekran końcowy
+        endLay = findViewById(R.id.endingLayout);
+        goToMenu=findViewById(R.id.goToMenu);
+        wordsCnt = findViewById(R.id.wordsCnt);
+        errCnt = findViewById(R.id.errCnt);
+        mostErrDef = findViewById(R.id.mostErrDefinition);
+        mostErrDesc = findViewById(R.id.mostErrDescription);
+
         db.open();
         Cursor cursor = db.getDataFromTable(tableName);
 
         cursor.moveToFirst();
+        int c = 0;
         while(!cursor.isAfterLast()) {
             defDescList.put(cursor.getString(1), cursor.getString(2));
             definitionsArray.add(cursor.getString(1));
+            definitionsArrayCopy.add(cursor.getString(1));
+            c++;
             cursor.moveToNext();
         }
         db.close();
 
+        //ekran końcowy
+        errorsCount = new int[c];
+        Arrays.fill(errorsCount, 0);//zainicjalizowane zerami
+
+
         readyButton.setOnClickListener(View -> {
-            check.setVisibility(View.VISIBLE);
+            check.setVisibility(android.view.View.VISIBLE);
             String insertedWord = definitionTextEdit.getText().toString();
 
             correctAfterCheck.setText(definitionsArray.get(currentWord));
@@ -74,6 +97,7 @@ public class LearnActivity extends AppCompatActivity {
             } else {
                 check.setBackground(ContextCompat.getDrawable(this, R.drawable.border_red));
                 insertedAfterCheck.setTextColor(ContextCompat.getColor(this, R.color.redBorder));
+                errorsCount[currentWord]++;
                 notCorrectCnt++;
                 wasGuessed = false;
             }
@@ -96,6 +120,13 @@ public class LearnActivity extends AppCompatActivity {
                 definitionTextEdit.setSelection(definitionTextEdit.getText().length());
            }
         });
+
+        goToMenu.setOnClickListener(View -> {
+            Intent intentGoToMenu = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intentGoToMenu);
+
+            finish();
+        });
     }
 
     private int displayRandom(int prev, boolean wasGuessed) {
@@ -104,26 +135,67 @@ public class LearnActivity extends AppCompatActivity {
         definitionTextEdit.setText("");
         definitionTextEdit.requestFocus();
 
-        if(definitionsArray.size() == 0) {
-            Toast.makeText(this, "Koniec nauki", Toast.LENGTH_LONG).show();
-            SystemClock.sleep(50);
+        if (wasGuessed && prev > -1 && prev < definitionsArray.size())
+            definitionsArray.remove(prev);
 
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
+        if(definitionsArray.toArray().length == 0) {
+            //Toast.makeText(this, "pusty", Toast.LENGTH_SHORT).show();
+            drawEnd();
 
-            finish();
+
+            return -1;
         }
 
-        if(wasGuessed && prev > -1 && prev < definitionsArray.size()) definitionsArray.remove(prev);
-
-        int rand = (int) ((Math.random() * 10000) % definitionsArray.size()) ;
+        int rand = (int) ((Math.random() * 10000) % definitionsArray.size());
         descriptionTextView.setText(defDescList.get(definitionsArray.get(rand)));
 
         remainText.setText("");
         remainText.setText(R.string.remain);
         remainText.append(" ");
-        remainText.append( String.valueOf(definitionsArray.toArray().length));
+        remainText.append(String.valueOf(definitionsArray.toArray().length));
 
         return rand;
+    }
+
+    private void drawEnd() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+
+        remainText.setVisibility(View.INVISIBLE);
+        endLay.setVisibility(android.view.View.VISIBLE);
+
+        //wyświetlenie podsumowania
+        //wyświetlenie ilości słów w zestawie
+        wordsCnt.setText(" ");
+        wordsCnt.append(String.valueOf(definitionsArrayCopy.toArray().length));
+        wordsCnt.append(" ");
+
+        //wyświetlenie ilości błędów
+        errCnt.setText(" ");
+        errCnt.append(String.valueOf(notCorrectCnt));
+        errCnt.append(" ");
+
+        //najczęściej popełniany błąd
+        if(notCorrectCnt != 0) {
+            int mostErrors = maxIndex(errorsCount);
+
+            mostErrDef.setText(definitionsArrayCopy.get(mostErrors));
+            mostErrDesc.setText(defDescList.get(definitionsArrayCopy.get(mostErrors)));
+        } else {
+            TextView t = findViewById(R.id.mostErrorsTextView);
+            t.setText("");
+        }
+    }
+
+    private int maxIndex(int[] array) {
+        if(array.length != 0) {
+            int max = 0;
+            for(int i = 1; i < array.length; i++) {
+                if(array[i] > array[max]) max = i;
+            }
+
+            return max;
+        }
+        return -1;
     }
 }
